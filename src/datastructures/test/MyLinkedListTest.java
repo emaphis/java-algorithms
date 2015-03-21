f20db988bd22c797990b26417b29452ae6198eea
Ed Maphis <emaphis85@gmail.com>
2015-03-21 12:13:40 -0400

    Start linked list experiment

---
+++ src/datastructures/test/MyLinkedListTest.java
@@ -0,0 +1,22 @@
+package datastructures.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import datastructures.MyLinkedList;
+
+public class MyLinkedListTest {
+
+	@Test
+	public void testMyLinkedListCreation() {
+		MyLinkedList ls = new MyLinkedList();
+		assertNotNull("Test MyLinkedList Existence", ls);
+	}
+	
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
