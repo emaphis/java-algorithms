a3b1b5d9c2ff95aae24398e15049c026d15d1ed7
Ed Maphis <emaphis85@gmail.com>
2018-03-19 19:27:33 -0400

    More project settings

---
+++ src/algo/ch1/sec1/basicprogrammingmodel/HelloWorld.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *  Name:    Ed Maphis
+ *  NetID:   emaphis
+ *
+ *  Description:  Basic Hello World program.
+ ******************************************************************************/
+
+package algo.ch1.sec1.basicprogrammingmodel;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
--- src/algo/ch1/sec1_basic_programming_model/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package algo.ch1.sec1_basic_programming_model;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-
-}
